bc6961e5211528224cb50569498e9fc77f50b2a7
asimov <seyranmustafa4@gmail.com>
2024-10-07 23:51:53 +0300

    middleware

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authSlice";
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "../features/authSlice"
+import stockReducer from "../features/stockSlice"
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage" // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
+    stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
-});
-export default store;
+})
+
+export const persistor = persistStore(store)
+export default store
