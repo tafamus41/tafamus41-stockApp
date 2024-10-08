0b806a4e1651907502fabf529be39232e1c2db73
asimov <seyranmustafa4@gmail.com>
2024-10-08 23:55:21 +0300

    firms component updated

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import useStockRequests from "../services/useStockRequests"
 
 import Typography from "@mui/material/Typography"
@@ -28,6 +28,16 @@ const Firms = () => {
   const { getStock } = useStockRequests()
   const { firms } = useSelector((state) => state.stock)
 
+  const initialState = { image: "", address: "", phone: "", name: "" }
+  const [data, setData] = useState(initialState)
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setData(initialState)
+  }
+
   //? Sayfa yüklendikten sonra firmları getir
   useEffect(() => {
     // getFirms()
@@ -41,15 +51,25 @@ const Firms = () => {
       <Typography variant="h5" color={"error"} mb={2}>
         Firms
       </Typography>
-      <Button variant="contained" sx={{ mb: 2 }}>
+      <Button variant="contained" sx={{ mb: 2 }} onClick={handleOpen}>
         NEW FIRM
       </Button>
 
-      <FirmModal />
+      <FirmModal
+        open={open}
+        handleClose={handleClose}
+        data={data}
+        setData={setData}
+      />
       <Grid container justifyContent={"center"} gap={2}>
         {firms?.map((firm, index) => (
           <Grid item key={index}>
-            <FirmCard firm={firm} />
+            <FirmCard
+              firm={firm}
+              handleOpen={handleOpen}
+              data={data}
+              setData={setData}
+            />
           </Grid>
         ))}
       </Grid>
